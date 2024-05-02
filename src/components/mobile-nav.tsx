58c772dd4f43cf7e5e092c18944a03de1f5337ee
ravi italiya <128232746+raviitaliya@users.noreply.github.com>
2024-05-02 13:40:50 +0530

    Update mobile-nav.tsx

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -24,17 +24,8 @@ export function MobileNav({ className }: { className?: string }) {
   const handleCloseSheet = () => setOpen(false);
 
   return (
-    <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger asChild>
-        <Button size="icon" variant="outline" className={className}>
-          <Menu className="h-4 w-4" />
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left">
-        <h2 className="mb-4 text-center text-lg font-semibold">
+    <h2 className="mb-4 text-center text-lg font-semibold">
           Reelgram
         </h2>
-          </SheetContent>
-    </Sheet>
   );
 }
