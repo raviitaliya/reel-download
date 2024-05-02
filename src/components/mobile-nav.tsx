b5e9f54de6fa6c392c2bba52beb8c26ec8d92c35
ravi italiya <italiyaravi704@gmail.com>
2024-05-02 13:48:44 +0530

    update

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -12,8 +12,7 @@ import { Button } from "@/components/ui/button";
 
 import { Menu } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { siteLinks } from "@/lib/constants";
+import {  siteLinks } from "@/lib/constants";
 import { Separator } from "./ui/separator";
 
 export function MobileNav({ className }: { className?: string }) {
@@ -24,8 +23,39 @@ export function MobileNav({ className }: { className?: string }) {
   const handleCloseSheet = () => setOpen(false);
 
   return (
-    <h2 className="mb-4 text-center text-lg font-semibold">
-          Reelgram
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline" className={className}>
+          <Menu className="h-4 w-4" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <h2 className="mb-4 text-center text-lg font-semibold">
+          Site Navigation
         </h2>
+        <ul className="mb-8 space-y-3">
+          {siteLinks.map((link) => (
+            <li key={link.href}>
+              <Button
+                variant={pathname === link.href ? "default" : "outline"}
+                asChild
+              >
+                <Link
+                  href={link.href}
+                  className="w-full"
+                  onClick={handleCloseSheet}
+                >
+                  {link.name}
+                </Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+        <Separator className="my-4" />
+        <h2 className="mb-4 text-center text-lg font-semibold">
+          External Links
+        </h2>
+      </SheetContent>
+    </Sheet>
   );
 }
