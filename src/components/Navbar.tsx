cac401c428e20efee4b2bcff61f2063bcf5cdd5a
dakshgoswami <154328930+dakshgoswami@users.noreply.github.com>
2024-05-04 14:04:14 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export function Navbar() {
         )}
       >
         <div className="hidden select-none text-xl font-extrabold uppercase md:block">
-          reelgram
+          ReelGram
         </div>
         <MobileNav className="md:hidden" />
         <div className="ml-auto flex items-center gap-4">
